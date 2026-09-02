235564f452cc415272222b1b019c36bb97f9cfa8
kirestein <erik.proenca2011@gmail.com>
2026-09-02 09:34:58 -0300

    Configurando a nossa chamada root com mais robustes

--- fastapi_todo/app.py
+++ fastapi_todo/app.py
@@ -3,12 +3,18 @@ Application entry point for the FastAPI Todo application.
 
 """
 
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title='FastAPI Todo',
+    description='A simple Todo application built with FastAPI',
+    version='1.0.0',
+)
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     """
     Root endpoint that returns a greeting message.
